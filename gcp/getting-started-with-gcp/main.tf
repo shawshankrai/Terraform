5fdec8666086b30884acde6e5423c1944ae03ae3
Shashank Rai <shashank.xx92@gmail.com>
2023-03-30 21:45:51 +0530

    Sample: gcloud auth

---
+++ gcp/getting-started-with-gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.59.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "terraform-gcp-382215"
+  region = "us-central1"
+  zone = "us-central1-a"
+}
+
+resource google_storage_bucket "cloud-storage-bucket" {
+  name = "shashank-iam-terraform-cloud-storage-bucket-1"
+  location = "US-CENTRAL1"
+}
